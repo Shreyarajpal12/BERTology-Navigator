6b2889aaacfe39a06eaa104b2d86d65994087902
Shreya Rajpal <shreyarajpal6@gmail.com>
2023-10-06 13:18:12 +0530

    submission updated

--- Single_Question/st3.py
+++ Single_Question/st3.py
@@ -1,106 +1,3 @@
-# import json
-# from SPARQLWrapper import SPARQLWrapper, JSON
-# from urllib.parse import urlparse
-
-# def is_url(value):
-#     if "http" in value:
-#         return True
-#     return False
-    
-# def fetch_label(entity):
-#     # If entity contains 'nodeID' or is not a valid URL, return entity as is
-#     if 'nodeID' in entity or not is_url(entity):
-#         return entity
-#     elif is_url(entity):
-
-#         sparql = SPARQLWrapper("https://dblp-kg.ltdemos.informatik.uni-hamburg.de/sparql")
-        
-#         query = f"""
-#             PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-#             PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-#             SELECT (?x0 AS ?value) WHERE {{
-#                 SELECT DISTINCT ?x0 WHERE {{
-#                     <{entity}> rdfs:label ?x0 .
-#                 }}
-#             }}
-#         """
-        
-#         sparql.setQuery(query)
-#         sparql.setReturnFormat(JSON)
-#         results = sparql.query().convert()
-#         labels = [result['value']['value'] for result in results['results']['bindings']]
-#         return labels[0] if labels else entity
-
-
-# def fetch_one_hop_relations(entity):
-#     sparql = SPARQLWrapper("https://dblp-kg.ltdemos.informatik.uni-hamburg.de/sparql")
-    
-#     # Relations where entity is the subject
-#     query1 = f"""
-#         SELECT ?p ?o WHERE {{
-#             <{entity}> ?p ?o .
-#         }}
-#     """
-    
-#     # Relations where entity is the object
-#     query2 = f"""
-#         SELECT ?s ?p WHERE {{
-#             ?s ?p <{entity}> .
-#         }}
-#     """
-    
-#     relations = []
-    
-#     for query in [query1, query2]:
-#         sparql.setQuery(query)
-#         sparql.setReturnFormat(JSON)
-#         results = sparql.query().convert()
-#         for result in results['results']['bindings']:
-#             s = result.get('s', {}).get('value', None)
-#             p = result.get('p', {}).get('value', None)
-#             o = result.get('o', {}).get('value', None)
-#             relations.append((s, p, o))
-            
-#     return relations
-
-# def main():
-#     entity = "https://dblp.org/pid/95/2265"
-#     question = "Show the Wikidata ID of the person Robert Schober." 
-    
-#     relations = fetch_one_hop_relations(entity)
-    
-#     labeled_pairs = []
-#     for relation in relations:
-#         s_label, p_label, o_label = None, None, None
-        
-#         if relation[0]:
-#             s_label = fetch_label(relation[0])
-#         if relation[1]:
-#             p_label = fetch_label(relation[1])
-#         if relation[2]:
-#             o_label = fetch_label(relation[2])
-            
-#         labeled_pairs.append({
-#             's': s_label,
-#            # 's_Rel':relation[0],
-#             'p': p_label,
-#             #'p_Rel':relation[1],
-#             'o': o_label,
-#             #'o_Rel':relation[2],
-#         })
-    
-#     output_data = {
-#         'question': question,
-#         'labeled_pairs': labeled_pairs
-#     }
-    
-#     with open('labeled_pairs.json', 'w') as json_file:
-#         json.dump(output_data, json_file, indent=4)
-
-# if __name__ == "__main__":
-#     main()
-    
-
 import json, re
 from SPARQLWrapper import SPARQLWrapper, JSON
 from urllib.parse import urlparse
@@ -142,86 +39,69 @@ def fetch_one_hop_relations_and_labels(entity):
         relations.append(((s, s_label), (p, p_label), (o, o_label)))
         # print(relations)
     return relations
-
 def valid_pair(pair, question):
+
+    clean_question = re.sub(r"'[^']*'", "", question).lower()
     # Condition 1
-    # Condition 2
-    # print(question.lower())
-    if "wikidata" in question.lower():
-        print("wikidata")
+    if "wikidata" in clean_question:
         if not ("wikidata" in pair['s'].lower() or "wikidata" in pair['o'].lower()):
             return False
         else:
             return True
-    clean_question = re.sub(r"'[^']*'", "", question).lower()
-
-    # Check condition
+    # Condition 2
     if (("author" in clean_question or "wr" in clean_question) and ("publi" in clean_question or "paper" in clean_question)):
-        print("author")
         if "autho" not in pair['p'].lower():
             return False
         else:
             return True
+    # Condition 3
     if ("authored by" in clean_question):
-        print("author")
         if "autho" not in pair['p'].lower():
             return False
         else:
             return True
-    
-
-    if "bibtex" in question.lower():
-        print("hi")
+    # Condition 4
+    if "bibtex" in clean_question:
         if not ("bibtex" in pair['p']):
             return False
         else:
             return True
-
-
-    # Condition 3
-    if "affiliat" in question:
-        # print("primary")
-
+    # Condition 5
+    if "affiliat" in clean_question:
         if not ("primary affiliation" in pair['p']):
             return False
         else:
             return True
-
-
-
-    if "orcid" in question.lower():
+    # Condition 6
+    if "orcid" in clean_question:
         if not ("orcid" in pair['p']):
             return False
         else:
             return True
-    if ("when" in question.lower()) or ("year" in question.lower()):
+    # Condition 7
+    if ("when" in clean_question) or ("year" in clean_question):
         if not ("year of publication" in pair['p']):
             return False
         else:
             return True
-
-    if ("venue" in question.lower()) or ("where" in question.lower()):
+    # Condition 8
+    if ("venue" in clean_question) or ("where" in clean_question):
         if not ("published in" == pair['p']):
-            print("hi")
             return False
         else:
             return True
-    if "webpage" in question.lower():
-        # print("web page")
-
+    # Condition 9
+    if "webpage" in clean_question:
         if "web page URL" in pair['p']:
             return True
         else:
             return False
     if "http://www.w3.org/1999/02/22-rdf-syntax" in pair['p']:
-        # print("syntax")
         return False
 
-    # Condition 5
+    # Condition 10
     if "nodeID" in pair['s'] or "nodeID" in pair['o']:
-        # print("nodeID")
         return False
-
     return True
 
 def main():
